3e933961168c54524db7d45a6ea02bcca8207e6e
zshn25 <zshn25@gmail.com>
2015-10-28 10:47:57 +0530

    Create 1.robot_cleaner_move_rotate.cpp

---
+++ src/1.robot_cleaner_move_rotate.cpp
@@ -0,0 +1,119 @@
+#include "ros/ros.h"
+#include "geometry_msgs/Twist.h"
+#include "turtlesim/Pose.h"
+#include <sstream>
+
+using namespace std;
+
+ros::Publisher velocity_publisher;
+
+
+const double PI = 3.14159265359;
+
+void move(double speed, double distance, bool isForward);
+void rotate(double angular_speed, double angle, bool cloclwise);
+double degrees2radians(double angle_in_degrees);
+
+int main(int argc, char **argv)
+{
+	// Initiate new ROS node named "talker"
+	ros::init(argc, argv, "turtlesim_cleaner");
+	ros::NodeHandle n;
+	double speed, angular_speed;
+	double distance, angle;
+	bool isForward, clockwise;
+
+	velocity_publisher = n.advertise<geometry_msgs::Twist>("/turtle1/cmd_vel", 1000);
+	ros::Rate loop_rate(10);
+
+	//	/turtle1/cmd_vel is the Topic name
+	//	/geometry_msgs::Twist is the msg type 
+	ROS_INFO("\n\n\n ********START TESTING*********\n");
+	cout<<"enter speed: ";
+	cin>>speed;
+	cout<<"enter distance: ";
+	cin>>distance;
+	cout<<"forward?: ";
+	cin>>isForward;
+	move(speed, distance, isForward);
+
+	cout<<"enter angular velocity: ";
+	cin>>angular_speed;
+	cout<<"enter angle: ";
+	cin>>angle;
+	cout<<"Clockwise?: ";
+	cin>>clockwise;
+	rotate(degrees2radians(angular_speed), degrees2radians(angle), clockwise);
+
+	ros::spin();
+
+	return 0;
+}
+
+void move(double speed, double distance, bool isForward){
+   geometry_msgs::Twist vel_msg;
+   //set a random linear velocity in the x-axis
+   if (isForward)
+	   vel_msg.linear.x =abs(speed);
+   else
+	   vel_msg.linear.x =-abs(speed);
+   vel_msg.linear.y =0;
+   vel_msg.linear.z =0;
+   //set a random angular velocity in the y-axis
+   vel_msg.angular.x = 0;
+   vel_msg.angular.y = 0;
+   vel_msg.angular.z =0;
+
+   double t0 = ros::Time::now().toSec();
+   double current_distance = 0.0;
+   ros::Rate loop_rate(100);
+   do{
+	   velocity_publisher.publish(vel_msg);
+	   double t1 = ros::Time::now().toSec();
+	   current_distance = speed * (t1-t0);
+	   ros::spinOnce();
+	   loop_rate.sleep();
+	   //cout<<(t1-t0)<<", "<<current_distance <<", "<<distance<<endl;
+   }while(current_distance<distance);
+   vel_msg.linear.x =0;
+   velocity_publisher.publish(vel_msg);
+
+}
+
+void rotate (double angular_speed, double relative_angle, bool clockwise){
+
+	geometry_msgs::Twist vel_msg;
+	   //set a random linear velocity in the x-axis
+	   vel_msg.linear.x =0;
+	   vel_msg.linear.y =0;
+	   vel_msg.linear.z =0;
+	   //set a random angular velocity in the y-axis
+	   vel_msg.angular.x = 0;
+	   vel_msg.angular.y = 0;
+	   if (clockwise)
+	   	vel_msg.angular.z =-abs(angular_speed);
+	   else
+	   	vel_msg.angular.z =abs(angular_speed);
+
+	   double t0 = ros::Time::now().toSec();
+	   double current_angle = 0.0;
+	   ros::Rate loop_rate(1000);
+	   do{
+		   velocity_publisher.publish(vel_msg);
+		   double t1 = ros::Time::now().toSec();
+		   current_angle = angular_speed * (t1-t0);
+		   ros::spinOnce();
+		   loop_rate.sleep();
+		   //cout<<(t1-t0)<<", "<<current_angle <<", "<<relative_angle<<endl;
+	   }while(current_angle<relative_angle);
+	   vel_msg.angular.z =0;
+	   velocity_publisher.publish(vel_msg);
+}
+
+/**
+ *  converts angles from degree to radians  
+ */
+
+double degrees2radians(double angle_in_degrees){
+	return angle_in_degrees *PI /180.0;
+}
